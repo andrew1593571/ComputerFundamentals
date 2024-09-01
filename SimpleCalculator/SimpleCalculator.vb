ef5172101eb9a2f1f50bdc6ec8afde8dd2e6232e
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-01 16:28:06 -0600

    Add Header

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -1,4 +1,10 @@
-﻿Module SimpleCalculator
+﻿'Andrew Keller
+'RCET2265
+'Fall 2024
+'Simple Calculator
+'https://github.com/andrew1593571/SimpleCalculator.git
+
+Module SimpleCalculator
 
     Sub Main()
 
