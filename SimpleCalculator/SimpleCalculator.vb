4eb5dc4e1904bae0d4ab7768b5692931f515974e
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-01 17:00:39 -0600

    Do Loop

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -14,36 +14,36 @@ Module SimpleCalculator
         Dim firstNumber As Integer
         Dim secondNumber As Integer
 
-        Console.Write("Choose a Number: ")
-        userInput = Console.ReadLine() 'Pulls the entered number from the console.
-        firstNumber = CInt(userInput) 'converts to integer and saves
 
-        Console.Write("Choose a Number: ")
-        userInput = Console.ReadLine() 'pulls number from console
-        secondNumber = CInt(userInput) 'converts to integer and saves
 
-        Console.WriteLine("Select an Option:" & vbNewLine &
+        Do
+
+            Console.Write("Choose a Number: ")
+            userInput = Console.ReadLine() 'Pulls the entered number from the console.
+            firstNumber = CInt(userInput) 'converts to integer and saves
+
+            Console.Write("Choose a Number: ")
+            userInput = Console.ReadLine() 'pulls number from console
+            secondNumber = CInt(userInput) 'converts to integer and saves
+
+            Console.WriteLine("Select an Option:" & vbNewLine &
                           "1. Sum" & vbNewLine &
                           "2. Product" & vbNewLine &
                           "3. Quit")
-        userInput = Console.ReadLine()
+            userInput = Console.ReadLine()
 
-        Select Case userInput
-            Case "1"
-                Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
-            Case "2"
-                Console.WriteLine($"{firstNumber} x {secondNumber} = {firstNumber * secondNumber}")
-            Case "3"
-                Console.WriteLine("Have a Nice Day! Press enter to exit")
-            Case Else
-                Console.WriteLine($"You entered {userInput}. {userInput} is not a valid option.")
-        End Select
+            Select Case userInput
+                Case "1"
+                    Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
+                Case "2"
+                    Console.WriteLine($"{firstNumber} x {secondNumber} = {firstNumber * secondNumber}")
+                Case "3"
+                    Console.WriteLine("Have a Nice Day! Press enter to exit")
+                Case Else
+                    Console.WriteLine($"You entered {userInput}. {userInput} is not a valid option.")
+            End Select
 
-
-        'Do
-
-
-        'Loop Until userInput = "3"
+        Loop Until userInput = "3"
         Console.ReadLine()
     End Sub
 
