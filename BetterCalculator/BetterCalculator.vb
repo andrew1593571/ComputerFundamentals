da18d1b210f491f95b846a1dee3869d111453dc0
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-08 21:35:18 -0600

    Add header and Options

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -1,4 +1,13 @@
-﻿Module BetterCalculator
+﻿'Andrew Keller
+'RCET2265
+'Fall 2024
+'Better Calculator
+'https://github.com/andrew1593571/BetterCalculator.git
+
+Option Explicit On
+Option Strict On
+
+Module BetterCalculator
 
     Sub Main()
 
