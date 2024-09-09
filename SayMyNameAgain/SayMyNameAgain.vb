7382a5f7a3639829c3f9d4d0e3aff3088266497e
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-08 21:10:38 -0600

    Header and Options

--- SayMyNameAgain/SayMyNameAgain.vb
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -1,4 +1,14 @@
-﻿Module SayMyNameAgain
+﻿'Andrew Keller
+'RCET2265
+'Fall 2024
+'Say My Name Again
+'https://github.com/andrew1593571/SayMyNameAgain.git
+
+Option Explicit On
+Option Compare Text
+Option Strict On
+
+Module SayMyNameAgain
 
     Sub Main()
 
