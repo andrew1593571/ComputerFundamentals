8156a7d04670cea2ec8d74fc5ca9fb1d87361f72
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-08 22:05:40 -0600

    Added GetUserNumber Function

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -10,7 +10,40 @@ Option Strict On
 Module BetterCalculator
 
     Sub Main()
+        Dim userInput As String = ""
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
 
+        firstNumber = GetUserNumber()
+        secondNumber = GetUserNumber()
+
+
+        Console.ReadLine()
     End Sub
 
+    ''' <summary>
+    ''' Function to ask the user for a number and returns. Loops until valid entry is received.
+    ''' </summary>
+    ''' <returns></returns>
+    Function GetUserNumber() As Integer
+        Dim userInput As String
+        Dim number As Integer
+        Dim exitLoop As Boolean
+
+        Do
+            Console.WriteLine("Choose a Number:")
+            userInput = Console.ReadLine()
+            Try
+                number = CInt(userInput)
+                exitLoop = True
+            Catch ex As Exception
+                Console.WriteLine($"You entered ""{userInput}"", please enter a whole number.")
+            End Try
+        Loop Until exitLoop
+
+        Return number
+
+    End Function
+
 End Module
+
