caf36a2ff0a7b21c6375d5407e43d7cb34efeddb
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-08 21:16:07 -0600

    Ask user for name. Store in userInput

--- SayMyNameAgain/SayMyNameAgain.vb
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -11,7 +11,22 @@ Option Strict On
 Module SayMyNameAgain
 
     Sub Main()
+        Dim continueLoop As Boolean
+        Dim userInput As String
 
+        Console.Write("Please enter your name: ")
+        userInput = Console.ReadLine()
+
+        Console.WriteLine(userInput)
+
+        'Select Case userInput
+        '    Case 
+        'End Select
+        'Do
+
+        'Loop Until continueLoop
+
+        Console.ReadLine()
     End Sub
 
 End Module
