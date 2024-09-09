798995a1e31cebbdde9597ac441b7b0966f5199a
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-08 22:33:24 -0600

    Add Option Select, Loop whole program

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -1,11 +1,12 @@
-﻿'Andrew Keller
+﻿Option Explicit On
+Option Strict On
+'Andrew Keller
 'RCET2265
 'Fall 2024
 'Better Calculator
 'https://github.com/andrew1593571/BetterCalculator.git
 
-Option Explicit On
-Option Strict On
+Imports System.Linq.Expressions
 
 Module BetterCalculator
 
@@ -13,9 +14,44 @@ Module BetterCalculator
         Dim userInput As String = ""
         Dim firstNumber As Integer
         Dim secondNumber As Integer
+        Dim validOption As Boolean
+        Dim quitEntered As Boolean
 
-        firstNumber = GetUserNumber()
-        secondNumber = GetUserNumber()
+
+        Do 'Loop until quit is selected
+
+            firstNumber = GetUserNumber()
+            secondNumber = GetUserNumber()
+
+            Do 'Loop until valid option is selected
+                Console.WriteLine($"Choose one of the following options:{vbNewLine}1. Add{vbNewLine}2. Subtract{vbNewLine}3. Multiply{vbNewLine}4. Divide{vbNewLine}5. Quit")
+                userInput = Console.ReadLine()
+                Console.WriteLine($"You entered ""{userInput}""")
+
+                Select Case userInput
+                    Case "1"
+                        Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
+                        validOption = True
+                    Case "2"
+                        Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}")
+                        validOption = True
+                    Case "3"
+                        Console.WriteLine($"{firstNumber} x {secondNumber} = {firstNumber * secondNumber}")
+                        validOption = True
+                    Case "4"
+                        Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}")
+                        validOption = True
+                    Case "5"
+                        Console.WriteLine($"Have a nice day!{vbNewLine}Press enter to close this window")
+                        validOption = True
+                        quitEntered = True
+                    Case Else
+
+                End Select
+
+            Loop Until validOption
+
+        Loop Until quitEntered
 
 
         Console.ReadLine()
@@ -35,6 +71,7 @@ Module BetterCalculator
             userInput = Console.ReadLine()
             Try
                 number = CInt(userInput)
+                Console.WriteLine($"You entered ""{userInput}""")
                 exitLoop = True
             Catch ex As Exception
                 Console.WriteLine($"You entered ""{userInput}"", please enter a whole number.")
