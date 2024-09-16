bfcc179260c9792206b454bb0d9790765a197090
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-15 21:09:34 -0600

    GetUserNumber Function

--- MultiplicationTable/MultiplicationTable.vb
+++ MultiplicationTable/MultiplicationTable.vb
@@ -1,5 +1,6 @@
 ﻿Option Strict On
 Option Explicit On
+Option Compare Text
 'Andrew Keller
 'RCET2265
 'Fall 2024
@@ -10,6 +11,33 @@ Module MultiplicationTable
 
     Sub Main()
 
+
+        Console.ReadLine()
     End Sub
+
+    ''' <summary>
+    ''' Requests a number from the user. Returns an Integer
+    ''' </summary>
+    ''' <returns></returns>
+    Function GetUserNumber() As Integer
+        Dim userInput As String
+        Dim number As Integer
+        Dim exitLoop As Boolean
+
+        Do
+            Console.WriteLine("Please enter a number:")
+            userInput = Console.ReadLine()
+            Try
+                number = CInt(userInput)
+                Console.WriteLine($"You entered ""{userInput}""")
+                exitLoop = True
+            Catch ex As Exception
+                Console.WriteLine($"You entered ""{userInput}"", please enter a whole number.")
+            End Try
+        Loop Until exitLoop
+
+        Return number
+
+    End Function
 
 End Module
