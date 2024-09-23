94365ff4bdd73de5cd1f29e03796d609a0c2ba8f
Andrew Keller <andrewkeller2002@gmail.com>
2024-09-22 21:39:58 -0600

    add header

--- ConvertAndValidate/Module1.vb
+++ ConvertAndValidate/Module1.vb
@@ -1,6 +1,12 @@
 ﻿Option Explicit On
 Option Strict On
 
+'Andrew Keller
+'RCET2265
+'Fall 2024
+'ConvertAndValidate
+'https://github.com/andrew1593571/ConvertAndValidate.git
+
 Module Module1
 
     Sub Main()
