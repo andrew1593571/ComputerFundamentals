57867d129c08234ceddb1663afe760bc766e2d50
Andrew Keller <andrewkeller2002@gmail.com>
2024-11-19 08:55:19 -0700

    Exit Button

--- DinerMenu/DinerMenuForm.vb
+++ DinerMenu/DinerMenuForm.vb
@@ -27,4 +27,8 @@
 
         DisplaySpecialLabel.Text = title & vbNewLine & name & vbNewLine & description
     End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 End Class
