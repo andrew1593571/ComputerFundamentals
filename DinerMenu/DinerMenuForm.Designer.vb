316c748e1754440ac17516163b0e702ded8b5138
Andrew Keller <andrewkeller2002@gmail.com>
2024-11-19 08:53:38 -0700

    Layout, Menu Item Buttons

--- DinerMenu/DinerMenuForm.Designer.vb
+++ DinerMenu/DinerMenuForm.Designer.vb
@@ -22,10 +22,99 @@ Partial Class DinerMenuForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(12, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(776, 39)
+        Me.DinerNameLabel.TabIndex = 0
+        Me.DinerNameLabel.Text = "The Cyber Cafe"
+        Me.DinerNameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.SoupButton.Location = New System.Drawing.Point(12, 378)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(130, 60)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.SaladButton.Location = New System.Drawing.Point(148, 378)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(130, 60)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.FishButton.Location = New System.Drawing.Point(284, 378)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(130, 60)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ExitButton.Location = New System.Drawing.Point(658, 378)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(130, 60)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(12, 48)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(776, 327)
+        Me.DisplaySpecialLabel.TabIndex = 4
+        Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Name = "DinerMenuForm"
+        Me.Text = "The Cyber Cafe Special Menu"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DisplaySpecialLabel As Label
 End Class
--- DinerMenu/DinerMenuForm.vb
+++ DinerMenu/DinerMenuForm.vb
@@ -1,3 +1,30 @@
 ﻿Public Class DinerMenuForm
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        Dim title As String = "~ Soup of the Day ~" & vbNewLine
+        Dim name As String = "Hacker's Delight" & vbNewLine
+        Dim description As String
+        description = "A savory blend of chicken, potatoes, and carrots seasoned with herbs and spices, all topped off with a rich cream sause. The perfect accompaniment to any digital conquest!"
 
+
+        DisplaySpecialLabel.Text = title & vbNewLine & name & vbNewLine & description
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        Dim title As String = "~ House Salad ~" & vbNewLine
+        Dim name As String = "Digital Delirium" & vbNewLine
+        Dim description As String
+        description = "A salad of mixed greens, crispy fried bacon, diced chicken, and shredded mozzarella cheese, topped with a drizzle of digital tears, a special sauce made from the finest algorithmically-generated spices."
+
+        DisplaySpecialLabel.Text = title & vbNewLine & name & vbNewLine & description
+    End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        Dim title As String = "~ Daily Fish ~" & vbNewLine
+        Dim name As String = "The Circuit Breaker" & vbNewLine
+        Dim description As String
+        description = " Crispy fried catfish that's been rebooted 17 times to achieve optimal crunchiness, served with a side of coleslaw that's been genetically engineered to have the perfect ratio of vinegar to sugar."
+
+
+        DisplaySpecialLabel.Text = title & vbNewLine & name & vbNewLine & description
+    End Sub
 End Class
