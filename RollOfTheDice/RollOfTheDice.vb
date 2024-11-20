e03fb54c32e0f5b9dac5c1fce878dde68a46ce02
Andrew Keller <andrewkeller2002@gmail.com>
2024-11-20 08:28:29 -0700

    GetRandomNumberInRange Function

--- RollOfTheDice/RollOfTheDice.vb
+++ RollOfTheDice/RollOfTheDice.vb
@@ -12,4 +12,21 @@ Module RollOfTheDice
 
     End Sub
 
+    ''' <summary>
+    ''' Returns a random number in the specified range
+    ''' </summary>
+    ''' <param name="Max"></param>
+    ''' <param name="Min"></param>
+    ''' <returns></returns>
+    Function GetRandomNumberInRange(Max As Integer, Optional Min As Integer = 0) As Integer
+        Dim randomNumber As Integer
+
+        'Initialize randomize with the current time in milliseconds
+        Randomize(DateTime.Now.Millisecond)
+
+        randomNumber = CInt(Math.Floor((Rnd() * ((Max - Min) + 1)))) + Min
+
+        Return randomNumber
+    End Function
+
 End Module
