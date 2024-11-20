362ad6477e8af3376a1846d2aa3b489e22388aaa
Andrew Keller <andrewkeller2002@gmail.com>
2024-11-20 08:56:35 -0700

    Add Header

--- RollOfTheDiceListBox/RollOfTheDiceListBoxForm.vb
+++ RollOfTheDiceListBox/RollOfTheDiceListBoxForm.vb
@@ -1,3 +1,11 @@
-﻿Public Class RollOfTheDiceListBoxForm
+﻿Option Explicit On
+Option Strict On
+
+'Andrew Keller
+'RCET2265
+'Fall 2024
+'https://github.com/andrew1593571/RollOfTheDiceListBox.git
+
+Public Class RollOfTheDiceListBoxForm
 
 End Class
