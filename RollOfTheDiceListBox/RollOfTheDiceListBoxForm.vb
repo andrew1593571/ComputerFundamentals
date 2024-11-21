e2feaa6babdf2303d4d78aacaa35812f41f9893a
Andrew Keller <andrewkeller2002@gmail.com>
2024-11-21 08:33:05 -0700

    RollButton Sub

--- RollOfTheDiceListBox/RollOfTheDiceListBoxForm.vb
+++ RollOfTheDiceListBox/RollOfTheDiceListBoxForm.vb
@@ -12,6 +12,47 @@ Public Class RollOfTheDiceListBoxForm
     End Sub
 
     Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim _columnWidth As Integer = 6
+        Dim _diceRolls(10) As Integer 'stores the number of rolls for each number 2 through 12
+        Dim _column As String
+        Dim _row As String
+        Dim _title As String = "Roll of the Dice"
+        Dim _message As String = ""
+
+        'finds the pad width to fit center the title over the table
+        Dim titlePad As Integer = (((_columnWidth * 11) - _title.Length) \ 2) + _title.Length
+
+        'roll the dice i times, increment the rolled value by 1
+        For i = 0 To 1000
+            _diceRolls(GetRandomNumberInRange(10)) += 1
+        Next
+
+        'Clear the ListBox
+        ResultsListBox.Items.Clear()
+
+        'Add in the title row
+        ResultsListBox.Items.Add(_title.PadLeft(titlePad))
+
+        'Add in the column header row
+        ResultsListBox.Items.Add(DrawLine(_columnWidth * 11))
+        _row = ""
+        For i = 0 To 10
+            _column = $"|  {CStr(i + 2)}".PadRight(_columnWidth)
+            _row &= _column
+        Next
+        ResultsListBox.Items.Add(_row & "|")
+
+        'Add in the Add in the dice roll results
+        ResultsListBox.Items.Add(DrawLine(_columnWidth * 11))
+        _row = ""
+        For i = 0 To 10
+            _column = $"| {CStr(_diceRolls(i))}".PadRight(_columnWidth)
+            _row &= _column
+        Next
+        ResultsListBox.Items.Add(_row & "|")
+
+        'Add in bottom line of table
+        ResultsListBox.Items.Add(DrawLine(_columnWidth * 11))
 
     End Sub
 
