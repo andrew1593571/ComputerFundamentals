5fe8c58a1a822f26faa7631f8d62d43fb57df288
Andrew Keller <andrewkeller2002@gmail.com>
2024-11-21 08:48:54 -0700

    Changed to Monospace Font, Updated Text Size

--- RollOfTheDiceListBox/RollOfTheDiceListBoxForm.Designer.vb
+++ RollOfTheDiceListBox/RollOfTheDiceListBoxForm.Designer.vb
@@ -31,7 +31,7 @@ Partial Class RollOfTheDiceListBoxForm
         'ExitButton
         '
         Me.ExitButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ExitButton.Location = New System.Drawing.Point(688, 378)
+        Me.ExitButton.Location = New System.Drawing.Point(712, 389)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(100, 60)
         Me.ExitButton.TabIndex = 0
@@ -41,7 +41,7 @@ Partial Class RollOfTheDiceListBoxForm
         'RollButton
         '
         Me.RollButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.RollButton.Location = New System.Drawing.Point(12, 378)
+        Me.RollButton.Location = New System.Drawing.Point(12, 389)
         Me.RollButton.Name = "RollButton"
         Me.RollButton.Size = New System.Drawing.Size(100, 60)
         Me.RollButton.TabIndex = 1
@@ -51,7 +51,7 @@ Partial Class RollOfTheDiceListBoxForm
         'ClearButton
         '
         Me.ClearButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.ClearButton.Location = New System.Drawing.Point(118, 378)
+        Me.ClearButton.Location = New System.Drawing.Point(118, 389)
         Me.ClearButton.Name = "ClearButton"
         Me.ClearButton.Size = New System.Drawing.Size(100, 60)
         Me.ClearButton.TabIndex = 2
@@ -63,18 +63,20 @@ Partial Class RollOfTheDiceListBoxForm
         Me.ResultsListBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ResultsListBox.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ResultsListBox.FormattingEnabled = True
+        Me.ResultsListBox.ItemHeight = 22
         Me.ResultsListBox.Location = New System.Drawing.Point(12, 12)
         Me.ResultsListBox.Name = "ResultsListBox"
         Me.ResultsListBox.SelectionMode = System.Windows.Forms.SelectionMode.None
-        Me.ResultsListBox.Size = New System.Drawing.Size(776, 355)
+        Me.ResultsListBox.Size = New System.Drawing.Size(800, 356)
         Me.ResultsListBox.TabIndex = 3
         '
         'RollOfTheDiceListBoxForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(824, 461)
         Me.Controls.Add(Me.ResultsListBox)
         Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.RollButton)
