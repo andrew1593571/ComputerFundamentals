2ad0c02d4b5796c04d8a6685fb48d3357967b2f3
Andrew Keller <andrewkeller2002@gmail.com>
2024-12-02 08:20:28 -0700

    Add project files.

---
+++ EtchASketch/EtchASketchForm.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class EtchASketchForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DrawingPictureBox = New System.Windows.Forms.PictureBox()
+        Me.ContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackgroundColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ControlsGroupBox = New System.Windows.Forms.GroupBox()
+        Me.WaveButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ColorDialog = New System.Windows.Forms.ColorDialog()
+        Me.SaveFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PenSizeLabel = New System.Windows.Forms.Label()
+        Me.PenSizeTrackBar = New System.Windows.Forms.TrackBar()
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip.SuspendLayout()
+        Me.ControlsGroupBox.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PenSizeTrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DrawingPictureBox
+        '
+        Me.DrawingPictureBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DrawingPictureBox.ContextMenuStrip = Me.ContextMenuStrip
+        Me.DrawingPictureBox.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.DrawingPictureBox.Location = New System.Drawing.Point(12, 12)
+        Me.DrawingPictureBox.Name = "DrawingPictureBox"
+        Me.DrawingPictureBox.Size = New System.Drawing.Size(788, 331)
+        Me.DrawingPictureBox.TabIndex = 0
+        Me.DrawingPictureBox.TabStop = False
+        '
+        'ContextMenuStrip
+        '
+        Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ColorToolStripMenuItem, Me.BackgroundColorToolStripMenuItem})
+        Me.ContextMenuStrip.Name = "ContextMenuStrip"
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(171, 48)
+        '
+        'ColorToolStripMenuItem
+        '
+        Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.ColorToolStripMenuItem.Text = "Color"
+        '
+        'BackgroundColorToolStripMenuItem
+        '
+        Me.BackgroundColorToolStripMenuItem.Name = "BackgroundColorToolStripMenuItem"
+        Me.BackgroundColorToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.BackgroundColorToolStripMenuItem.Text = "Background Color"
+        '
+        'ControlsGroupBox
+        '
+        Me.ControlsGroupBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ControlsGroupBox.Controls.Add(Me.PenSizeLabel)
+        Me.ControlsGroupBox.Controls.Add(Me.PenSizeTrackBar)
+        Me.ControlsGroupBox.Controls.Add(Me.WaveButton)
+        Me.ControlsGroupBox.Controls.Add(Me.ClearButton)
+        Me.ControlsGroupBox.Controls.Add(Me.ExitButton)
+        Me.ControlsGroupBox.Location = New System.Drawing.Point(12, 349)
+        Me.ControlsGroupBox.Name = "ControlsGroupBox"
+        Me.ControlsGroupBox.Size = New System.Drawing.Size(788, 100)
+        Me.ControlsGroupBox.TabIndex = 1
+        Me.ControlsGroupBox.TabStop = False
+        '
+        'WaveButton
+        '
+        Me.WaveButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WaveButton.Location = New System.Drawing.Point(386, 19)
+        Me.WaveButton.Name = "WaveButton"
+        Me.WaveButton.Size = New System.Drawing.Size(128, 75)
+        Me.WaveButton.TabIndex = 2
+        Me.WaveButton.Text = "&Wave"
+        Me.WaveButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ClearButton.Location = New System.Drawing.Point(520, 19)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(128, 75)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ExitButton.Location = New System.Drawing.Point(654, 19)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(128, 75)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog1"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileTopMenuItem, Me.HelpTopMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(812, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileTopMenuItem
+        '
+        Me.FileTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenTopMenuItem, Me.SaveTopMenuItem})
+        Me.FileTopMenuItem.Name = "FileTopMenuItem"
+        Me.FileTopMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileTopMenuItem.Text = "&File"
+        '
+        'OpenTopMenuItem
+        '
+        Me.OpenTopMenuItem.Name = "OpenTopMenuItem"
+        Me.OpenTopMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OpenTopMenuItem.Text = "&Open"
+        '
+        'SaveTopMenuItem
+        '
+        Me.SaveTopMenuItem.Name = "SaveTopMenuItem"
+        Me.SaveTopMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SaveTopMenuItem.Text = "&Save"
+        '
+        'HelpTopMenuItem
+        '
+        Me.HelpTopMenuItem.Name = "HelpTopMenuItem"
+        Me.HelpTopMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpTopMenuItem.Text = "&Help"
+        '
+        'PenSizeLabel
+        '
+        Me.PenSizeLabel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PenSizeLabel.Location = New System.Drawing.Point(249, 19)
+        Me.PenSizeLabel.Name = "PenSizeLabel"
+        Me.PenSizeLabel.Size = New System.Drawing.Size(131, 27)
+        Me.PenSizeLabel.TabIndex = 4
+        Me.PenSizeLabel.Text = "Pen Size"
+        Me.PenSizeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PenSizeTrackBar
+        '
+        Me.PenSizeTrackBar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PenSizeTrackBar.Location = New System.Drawing.Point(252, 49)
+        Me.PenSizeTrackBar.Minimum = 1
+        Me.PenSizeTrackBar.Name = "PenSizeTrackBar"
+        Me.PenSizeTrackBar.Size = New System.Drawing.Size(128, 45)
+        Me.PenSizeTrackBar.TabIndex = 3
+        Me.PenSizeTrackBar.Value = 1
+        '
+        'GraphicsExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(812, 461)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.ControlsGroupBox)
+        Me.Controls.Add(Me.DrawingPictureBox)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "GraphicsExampleForm"
+        Me.Text = "Form1"
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip.ResumeLayout(False)
+        Me.ControlsGroupBox.ResumeLayout(False)
+        Me.ControlsGroupBox.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PenSizeTrackBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DrawingPictureBox As PictureBox
+    Friend WithEvents ControlsGroupBox As GroupBox
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ColorDialog As ColorDialog
+    Friend WithEvents ContextMenuStrip As ContextMenuStrip
+    Friend WithEvents ColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BackgroundColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WaveButton As Button
+    Friend WithEvents SaveFileDialog As SaveFileDialog
+    Friend WithEvents OpenFileDialog As OpenFileDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileTopMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenTopMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveTopMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpTopMenuItem As ToolStripMenuItem
+    Friend WithEvents PenSizeLabel As Label
+    Friend WithEvents PenSizeTrackBar As TrackBar
+End Class
